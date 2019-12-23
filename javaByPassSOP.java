f8e89b52d5b6c0d18d7c307a620ef853f7ffa2b8
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2019-12-23 09:16:28 +0800

    可作用於 PDF Reader 等 Plugin

---
+++ javaByPassSOP.java
@@ -0,0 +1,32 @@
+import java.applet.*;
+
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+import java.awt.*;
+
+public class javaByPassSOP extends Applet{
+
+    public javaByPassSOP(){
+        super();
+        return;
+    }
+
+    public static String getInfo(){
+
+        String result = "";
+
+        try {
+
+        } catch(Exception e) {
+
+            result = "e:" + e.toString();
+
+        }
+
+        return result;
+
+    }
+}
+
